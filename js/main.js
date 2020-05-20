875dddce76b9ceb0c041cf97f067d2c1202cbb9f
Moshiur <codermoshiur@gmail.com>
2020-05-20 06:12:08 +0600

    first commit

--- js/main.js
+++ js/main.js
@@ -1,26 +1,5 @@
-$(document).ready(function () {
-  var mySwiper = new Swiper ('.swiper-container', {
-      lazy: true,
-      effect: 'cube',
-      grabCursor: true,
-      cubeEffect: {
-        shadow: true,
-        slideShadows: true,
-        shadowOffset: 20,
-        shadowScale: 0.94,
-      },
-      loop: true, 
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },    
-      pagination: {
-        el: '.swiper-pagination',
-        type: 'fraction',
-      },
-      speed: 1000,
-      autoplay: {
-        delay: 3000,
-      },
-  });
-});
+$(function() {
+    $('.header').animatedHeadline({
+        animationType: 'clip'
+    });
+})
